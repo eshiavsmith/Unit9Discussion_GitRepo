39a9c9a5ee65c70e91ddd73222f52a1500f224f0
Eshia Smith <eshiavsmith@hotmail.com>
2021-03-12 23:52:34 -0500

    Made adjustments to the code

---
+++ Unit9Discussion_GitRepo/Unit9Discussion_GitRepo/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Unit9Discussion_GitRepo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
